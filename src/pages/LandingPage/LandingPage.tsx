657d2f6f82ecaeb15500bad037662a05afcf13a7
Nobody-123 <dikhyantkrishnadalai@gmail.com>
2024-02-04 23:55:21 +0530

    Code rectification

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -3,6 +3,9 @@ import styles from "./styles.module.css";
 import BlueFaceImage from "../../assets/images/blue-face.png";
 import PrimaryButton from "../../components/common/PrimaryButton/PrimaryButton";
 import SecondaryButton from "../../components/common/SecondaryButton/SecondaryButton";
+import FirstRoundImage from "../../assets/images/first_round.png";
+import EightVCImage from "../../assets/images/eight_vc.png";
+import StartXImage from "../../assets/images/startX.png";
 
 type LandingPageProps = {
 
@@ -16,6 +19,12 @@ const LandingPage: React.FC<LandingPageProps> = () => {
             <section
                 className={`${styles.contentContainer}`}
                 id={`${styles.heroSection}`}
+                style={{
+                    ...{
+                        marginTop: 20,
+                        paddingBottom: 500,
+                    }
+                }}
             >
                 <div>
                     <h1
@@ -99,6 +108,75 @@ const LandingPage: React.FC<LandingPageProps> = () => {
                     src={BlueFaceImage}
                     id={`${styles.heroImage}`}
                 />
+            </section>
+
+            <section
+                id={`${styles.investorsSection}`}
+                style={{
+                    ...{
+                        marginTop: 20,
+                        display: "none",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        // justifyContent: "center"?
+                    }
+                }}
+            >
+                <h2
+                    className={`${styles.sectionHeading}`}
+                    style={{
+                        ...{
+                            color: "#fff",
+                            fontWeight: "400",
+                        }
+                    }}
+                >
+                Backed by world-class investors and partners
+                </h2>
+
+                <div
+                    className={`${styles.buttonContainer}`}
+                    style={{
+                        ...{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "space-between",
+                            marginTop: 20,
+                            // paddingBottom: 50,
+                        }
+                    }}
+                >
+                    <button
+                        className={`${styles.companyButton}`}
+                    >
+                        <img
+                            src={FirstRoundImage}
+                        />
+                    </button>
+
+                    <button
+                        className={`${styles.companyButton}`}
+                    >
+                        <img
+                            src={EightVCImage}
+                        />
+                    </button>
+
+                    <button
+                        className={`${styles.companyButton}`}
+                    >
+                        <img
+                            src={StartXImage}
+
+                        />
+                    </button>
+
+                    <h3
+                        className={`${styles.moreText}`}
+                    >
+                        +33 more
+                    </h3>
+                </div>
             </section>
         </div>
     )
